7cbe11bb2fa1c7d3c646a4993fab423bd6c3bbea
tasmia-rahman <maliha1288@gmail.com>
2022-12-29 22:48:06 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,12 +14,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aw5zzia.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
 
     try {
         const usersCollection = client.db('postbookDB').collection('users');
         const postsCollection = client.db('postbookDB').collection('posts');
         const commentsCollection = client.db('postbookDB').collection('comments');
+
+        //JWT
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ token });
+        })
 
         //Users
         app.get('/users/:email', async (req, res) => {
@@ -72,7 +97,7 @@ async function run() {
             res.send(post);
         });
 
-        app.post('/posts', async (req, res) => {
+        app.post('/posts', verifyJWT, async (req, res) => {
             const post = req.body;
             post.loveCount = 0;
             const result = await postsCollection.insertOne(post);
@@ -132,7 +157,7 @@ async function run() {
         });
 
         //Details
-        app.get('/details/:id', async (req, res) => {
+        app.get('/details/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await postsCollection.findOne(filter);
